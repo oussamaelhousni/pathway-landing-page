455828b67e4f9b6bbf2fb45bff6ce01d3574715f
Oussama Elhousni <oussamaelhousnioe@gmail.com>
2024-11-23 13:27:15 +0100

    add:animation to hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import Image from "next/image";
-
+import { motion, useScroll, useTransform } from "framer-motion";
 import heroImage from "@/assets/cog.png";
 import cylinder from "@/assets/cylinder.png";
 import noodlze from "@/assets/noodle.png";
 import Button from "./button";
 import LinkWithArrow from "./link-with-arrow";
 const Hero = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
   return (
-    <section className="py-[3rem] bg-gradient-to-tr from-blue-500/30 from 50%% via-blue-200/50 via-50% to-70% to-white overflow-hidden">
+    <section
+      className="py-[3rem] bg-gradient-to-tr from-blue-500/30 from 50%% via-blue-200/50 via-50% to-70% to-white"
+      ref={ref}
+    >
       <div className="container mx-auto mt-20 flex flex-col gap-16 md:mt-0 md:flex-row md:items-center">
         <div className="space-y-8 sm:space-y-12 sm:w-[470px] lg:w-[600px]">
           <div className="text-sm border border-black/10 px-2 py-1 rounded-lg w-fit ">
@@ -18,7 +29,7 @@ const Hero = () => {
             Pathway to productivity
           </h1>
 
-          <p className="text-[#010D3E] md:text-lg">
+          <p className="description text-left">
             Celebrate the joy of accomplishment with an app designed to track
             your progress, motivate your efforts, and celebrate your successes.
           </p>
@@ -29,23 +40,34 @@ const Hero = () => {
           </div>
         </div>
         <div className="relative md:flex-1 mt-8 md:h-[648px]">
-          <Image
-            src={cylinder}
+          <motion.img
+            src={cylinder.src}
             alt="cylandre"
             className="hidden md:block md:absolute md:-left-32 md:-top-12"
             width={220}
+            style={{ translateY: translateY }}
           />
-          <Image
-            src={heroImage}
+          <motion.img
+            src={heroImage.src}
             alt="her image"
-            className="md:block md:absolute md:max-w-none md:h-full md:w-auto xl:left-1/2 xl:-translate-x-1/3"
+            className="md:block md:absolute md:max-w-none md:h-full md:w-auto xl:right-[0]"
+            animate={{
+              translateY: [-30, 30],
+            }}
+            transition={{
+              duration: 3,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "mirror",
+            }}
           />
 
-          <Image
-            src={noodlze}
+          <motion.img
+            src={noodlze.src}
             alt="nooodlze"
-            className="hidden lg:block lg:absolute lg:-bottom-[200px] lg:left-[250px] xl:left-[648px] rotate-[30deg]"
+            className="hidden lg:block lg:absolute lg:-bottom-[200px] lg:left-[250px] xl:left-[648px]"
             width={220}
+            style={{ rotate: "30deg", translateY: translateY }}
           />
         </div>
       </div>
