2d1f813f696bf555b866b30cd9403a94cb3e79e4
Oussama Elhousni <oussamaelhousnioe@gmail.com>
2024-11-23 13:38:16 +0100

    logo tikcer animation

--- components/logo-ticker.tsx
+++ components/logo-ticker.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
@@ -5,26 +6,52 @@ import celestialLogo from "@/assets/logo-celestial.png";
 import echoLogo from "@/assets/logo-echo.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import quantumLogo from "@/assets/logo-quantum.png";
-
+import { motion } from "framer-motion";
 const Logoticker = () => {
   return (
     <section className=" bg-white py-8 md:py-12">
       <div className="container mx-auto">
-        <div className="flex flex-nowrap gap-14 overflow-x-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <Image src={acmeLogo} className="h-8 w-auto" alt={"acme Logo"} />
-          <Image src={apexLogo} className="h-8 w-auto" alt={"apex Logo"} />
-          <Image
-            src={celestialLogo}
-            className="h-8 w-auto"
-            alt={"celestial Logo"}
-          />
-          <Image src={echoLogo} className="h-8 w-auto" alt={"echo Logo"} />
-          <Image src={pulseLogo} className="h-8 w-auto" alt={"Pulse Logo"} />
-          <Image
-            src={quantumLogo}
-            className="h-8 w-auto"
-            alt={"quantum Logo"}
-          />
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <motion.div
+            className="flex items-center flex-none gap-16 pr-16"
+            animate={{ translateX: "-50%" }}
+            transition={{
+              duration: 10,
+              repeatType: "loop",
+              repeat: Infinity,
+              ease: "linear",
+            }}
+          >
+            <Image src={acmeLogo} className="h-8 w-auto" alt={"acme Logo"} />
+            <Image src={apexLogo} className="h-8 w-auto" alt={"apex Logo"} />
+            <Image
+              src={celestialLogo}
+              className="h-8 w-auto"
+              alt={"celestial Logo"}
+            />
+            <Image src={echoLogo} className="h-12 w-auto" alt={"echo Logo"} />
+            <Image src={pulseLogo} className="h-8 w-auto" alt={"Pulse Logo"} />
+            <Image
+              src={quantumLogo}
+              className="h-8 w-auto"
+              alt={"quantum Logo"}
+            />
+            {/*Next set of logos*/}
+            <Image src={acmeLogo} className="h-8 w-auto" alt={"acme Logo"} />
+            <Image src={apexLogo} className="h-8 w-auto" alt={"apex Logo"} />
+            <Image
+              src={celestialLogo}
+              className="h-8 w-auto"
+              alt={"celestial Logo"}
+            />
+            <Image src={echoLogo} className="h-12 w-auto" alt={"echo Logo"} />
+            <Image src={pulseLogo} className="h-8 w-auto" alt={"Pulse Logo"} />
+            <Image
+              src={quantumLogo}
+              className="h-8 w-auto"
+              alt={"quantum Logo"}
+            />
+          </motion.div>
         </div>
       </div>
     </section>
